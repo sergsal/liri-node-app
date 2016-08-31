4d9ca48a97fce805cd0800680c9b7856987e64e7
Sergio Saldarriaga <sergio.saldarriaga88@gmail.com>
2016-08-31 13:43:28 -0400

    includes twitter command functionality

--- liri.js
+++ liri.js
@@ -1,18 +1,56 @@
 // requiring keys.js to read keys.js file data
 var keys = require('./keys.js');
-// requiring NPMs for twitter, spotify and request
+// requiring NPMs for fs, twitter, spotify and request
+var fs = require('fs');
 var twitter = require('twitter');
 var spotify = require('spotify');
 var request = require('request');
 
-//assinging keys to variables for twitter call
-var consKey = keys.twitterKeys.consumer_key;
-var consSecret = keys.twitterKeys.consumer_secret;
-var accessKey = keys.twitterKeys.access_token_key;
-var accessSecret = keys.twitterKeys.access_token_secret;
-console.log(consKey+"\n"+consSecret+"\n"+accessKey+"\n"+accessSecret)
+//assinging keys for twitter call
+var client = new twitter({
+				consumer_key: keys.twitterKeys.consumer_key,
+				consumer_secret: keys.twitterKeys.consumer_secret,
+				access_token_key: keys.twitterKeys.access_token_key,
+				access_token_secret: keys.twitterKeys.access_token_secret,
+});
 
 //for recording user command
-var userInput = process.argv[2]
-//for recording user input such as song or movie
-for 
+var userCommand = process.argv[2];
+var nodeArgs = process.argv;
+var searchItem = "";
+//for recording user input such as song or movie, starting at 3 to capture search term, index 2 is for command
+for (var i=3; i<nodeArgs.length; i++){
+
+  // Build a string for the search term
+	searchItem += "" + nodeArgs[i];
+};
+console.log(searchItem);
+switch(userCommand) {
+								case("my-tweets"):
+								var params = {screen_name: 'zachlowe_nba'};
+								client.get('statuses/user_timeline', params, function(error, tweets, response) {
+  						if (!error) {
+												for (i=0; i<20; i++){
+																console.log(tweets[i].text);
+												}
+								}		
+});
+								break;
+								case("spotify-this-song"):
+								spotify.search({ type: 'track', query: searchItem }, function(err, data) {
+												if ( err ) {
+																console.log('Error occurred: ' + err);
+																return;
+												}
+ 
+    // Do something with 'data' 
+});
+								break;
+								case("movie-ths"):
+								//holder;
+								break;
+								case("do-what-it-says"):
+								//holder;
+								break;
+								
+}
